0a908984f3876e86ca82485f41509e9c7b9d669b
newgirll <k.kowalska2407@gmail.com>
2018-02-15 21:16:28 +0100

    add second task

---
+++ 02_Clock/app.js
@@ -0,0 +1,23 @@
+
+function setTime(){
+   const timeNow = new Date();
+   const seconds = timeNow.getSeconds();
+   const secondsHand = document.querySelector(".second-hand");
+   const secondDegree = ((seconds / 60) * 360) + 90;
+   secondsHand.style.transform = `rotate(${secondDegree}deg)`;
+   console.log(seconds)
+   console.log(secondDegree)
+
+   const minutes = timeNow.getMinutes();
+   const minuteDegree = ((minutes / 60) * 360) + 90;
+   const minuteHand = document.querySelector('.min-hand');
+   minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
+
+
+   const hour = timeNow.getHours();
+   const hourDegree = ((hour / 12) * 360) + 90;
+   const hourHand = document.querySelector(".hour-hand");
+   hourHand.style.transform = `rotate(${hourDegree}deg)`;
+}
+
+setInterval((setTime),1000);
